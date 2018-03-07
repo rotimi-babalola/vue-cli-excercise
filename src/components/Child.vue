c40cf36fba9a221a8872dea4de97eba9f4d1c08a
rotimi-babalola <rotimi.babalola@andela.com>
2018-03-07 17:40:54 +0100

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="place">
+        <img :src="image" width="235" height="300" />
+        <h2>{{ name }}</h2>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['image', 'description', 'name'],
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        This is just a test component
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
